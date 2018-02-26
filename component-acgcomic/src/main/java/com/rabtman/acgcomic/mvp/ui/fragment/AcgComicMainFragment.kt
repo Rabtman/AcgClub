b4156b548272e1229775a8f9379f80e5090d2be6
rabtman <zhangjm05@gmail.com>
2018-02-26 22:42:57 +0800

    完善漫画模块

--- component-acgcomic/src/main/java/com/rabtman/acgcomic/mvp/ui/fragment/AcgComicMainFragment.kt
+++ component-acgcomic/src/main/java/com/rabtman/acgcomic/mvp/ui/fragment/AcgComicMainFragment.kt
@@ -1,8 +1,13 @@
 package com.rabtman.acgcomic.mvp.ui.fragment
 
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import butterknife.BindView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.rabtman.acgcomic.R
@@ -16,6 +21,7 @@ import com.rabtman.common.base.BaseFragment
 import com.rabtman.common.base.widget.DropDownMenu
 import com.rabtman.common.di.component.AppComponent
 
+
 /**
  * @author Rabtman
  */
@@ -23,19 +29,21 @@ class AcgComicMainFragment : BaseFragment<ComicMainPresenter>(), ComicMainContra
 
     @BindView(R.id.ddm_acg_comic_menu)
     lateinit var mMenuAcgComic: DropDownMenu
-    private var headers = arrayOf("题材", "读者群", "进度", "地域", "排序")
+    private val headers = listOf("题材", "读者群", "进度", "地域", "排序")
     //菜单选项
-    private var topic = arrayListOf(
+    private val topic = arrayListOf(
             "全部", "冒险", "欢乐向", "格斗",
             "科幻", "爱情", "竞技", "魔法",
             "校园", "悬疑", "恐怖", "生活亲情",
             "百合", "伪娘", "耽美", "后宫",
             "萌系", "治愈", "武侠", "职场",
             "奇幻", "节操", "轻小说", "搞笑")
-    private var groups = arrayListOf("全部", "少年", "少女", "青年")
-    private var status = arrayListOf("全部", "连载", "完结")
-    private var area = arrayListOf("全部", "日本", "内地", "欧美", "港台", "韩国", "其他")
-    private var sort = arrayListOf("人气", "更新")
+    private val groups = arrayListOf("全部", "少年", "少女", "青年")
+    private val status = arrayListOf("全部", "连载", "完结")
+    private val area = arrayListOf("全部", "日本", "内地", "欧美", "港台", "韩国", "其他")
+    private val sort = arrayListOf("人气", "更新")
+    //当前菜单的选择结果
+    private var selected = arrayOf(0, 0, 0, 0, 0)
     //菜单选项适配器
     private var topicAdapter: ComicMenuAdapter? = null
     private var groupAdapter: ComicMenuAdapter? = null
@@ -58,21 +66,81 @@ class AcgComicMainFragment : BaseFragment<ComicMainPresenter>(), ComicMainContra
     }
 
     override fun initData() {
+        //题材
+        val topicView = RecyclerView(this.context)
+        topicView.setBackgroundColor(ContextCompat.getColor(this.context, R.color.grey200))
         topicAdapter = ComicMenuAdapter(topic)
-        topicAdapter!!.setOnItemClickListener(BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
+        topicAdapter!!.onItemClickListener = BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
             if (adapter is ComicMenuAdapter) {
                 adapter.setCheckItem(position)
                 mMenuAcgComic.setTabText(if (position == 0) headers[0] else topic[position])
+                mMenuAcgComic.closeMenu()
             }
-        })
-        var topicView = RecyclerView(this.context)
+        }
         topicView.layoutManager = GridLayoutManager(this.context, 4)
         topicView.adapter = topicAdapter
+        //读者群
+        val groupView = RecyclerView(this.context)
+        groupView.setBackgroundColor(ContextCompat.getColor(this.context, R.color.grey200))
+        groupAdapter = ComicMenuAdapter(groups)
+        groupAdapter!!.onItemClickListener = BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
+            if (adapter is ComicMenuAdapter) {
+                adapter.setCheckItem(position)
+                mMenuAcgComic.setTabText(if (position == 0) headers[1] else groups[position])
+                mMenuAcgComic.closeMenu()
+            }
+        }
+        groupView.layoutManager = GridLayoutManager(this.context, 4)
+        groupView.adapter = groupAdapter
+        //进度
+        val statusView = RecyclerView(this.context)
+        statusView.setBackgroundColor(ContextCompat.getColor(this.context, R.color.grey200))
+        statusAdapter = ComicMenuAdapter(status)
+        statusAdapter!!.onItemClickListener = BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
+            if (adapter is ComicMenuAdapter) {
+                adapter.setCheckItem(position)
+                mMenuAcgComic.setTabText(if (position == 0) headers[2] else status[position])
+                mMenuAcgComic.closeMenu()
+            }
+        }
+        statusView.layoutManager = GridLayoutManager(this.context, 4)
+        statusView.adapter = statusAdapter
+        //地域
+        val areaView = RecyclerView(this.context)
+        areaView.setBackgroundColor(ContextCompat.getColor(this.context, R.color.grey200))
+        areaAdapter = ComicMenuAdapter(area)
+        areaAdapter!!.onItemClickListener = BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
+            if (adapter is ComicMenuAdapter) {
+                adapter.setCheckItem(position)
+                mMenuAcgComic.setTabText(if (position == 0) headers[3] else area[position])
+                mMenuAcgComic.closeMenu()
+            }
+        }
+        areaView.layoutManager = GridLayoutManager(this.context, 4)
+        areaView.adapter = areaAdapter
+        //排序
+        /*val sortView = RecyclerView(this.context)
+        sortView.setBackgroundColor(ContextCompat.getColor(this.context, R.color.grey200))
+        sortAdapter = ComicMenuAdapter(sort)
+        sortAdapter!!.onItemClickListener = BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
+            if (adapter is ComicMenuAdapter) {
+                adapter.setCheckItem(position)
+                mMenuAcgComic.setTabText(if (position == 0) headers[4] else sort[position])
+                mMenuAcgComic.closeMenu()
+            }
+        }
+        sortView.layoutManager = GridLayoutManager(this.context, 4)
+        sortView.adapter = sortAdapter*/
 
-        var groupView = RecyclerView(this.context)
-        var statusView = RecyclerView(this.context)
-        var areaView = RecyclerView(this.context)
-        var sortView = RecyclerView(this.context)
+        //init context view
+        val contentView = TextView(this.context)
+        contentView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        contentView.text = "内容显示区域"
+        contentView.gravity = Gravity.CENTER
+        contentView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+
+        popupViews = listOf<View>(topicView, groupView, statusView, areaView)
+        mMenuAcgComic.setDropDownMenu(headers, popupViews!!, contentView)
     }
 
     override fun showComicInfos(comicInfos: List<AcgComicItem>) {
